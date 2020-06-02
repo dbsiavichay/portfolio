eb9f343095814fe337bc308ce821d94439851e00
Denis Siavichay <dsiavichay@mmorona.gob.ec>
2020-06-01 23:59:18 -0500

    ✨ Set Resume admin models

--- apps/resume/admin.py
+++ apps/resume/admin.py
@@ -1,3 +1,37 @@
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# Models
+from apps.resume.models import JobTitle, JobExperience, JobActivity
+
+
+@admin.register(JobTitle)
+class JobTitleAdmin(admin.ModelAdmin):
+    list_display = ('level', 'institution', 'name')
+    fields = (
+        'level',
+        'institution',
+        'name',
+        'senescyt_number',
+        'start_date',
+        'ends_date'
+    )
+
+
+class JobActivityInline(admin.TabularInline):
+    model = JobActivity
+
+
+@admin.register(JobExperience)
+class JobExperienceAdmin(admin.ModelAdmin):
+    list_display = ('institution', 'job_position', 'current_job')
+    fields = (
+        'institution',
+        'job_position',
+        'start_date',
+        'ends_date',
+        'current_job'
+    )
+    inlines = (
+        JobActivityInline,
+    )
--- apps/resume/models.py
+++ apps/resume/models.py
@@ -3,29 +3,59 @@ from django.db import models
 
 
 class Timeline(models.Model):
-    start_date = models.DateField()
-    ends_date = models.DateField(null=True)
+    start_date = models.DateField(verbose_name='fecha de inicio')
+    ends_date = models.DateField(
+        blank=True,
+        null=True,
+        verbose_name='fecha de finalización'
+    )
 
 
 class JobTitle(Timeline):
-    level = models.CharField(max_length=64)
-    name = models.CharField(max_length=128)
-    institution = models.CharField(max_length=128)
-    senescyt_number = models.CharField(max_length=64, blank=True, null=True)
+    level = models.CharField(
+        max_length=64,
+        verbose_name='nivel de instrucción'
+    )
+    institution = models.CharField(max_length=128, verbose_name='institución')
+    name = models.CharField(
+        max_length=128,
+        verbose_name='título obtenido'
+    )
+    senescyt_number = models.CharField(
+        max_length=64,
+        blank=True,
+        null=True,
+        verbose_name='registro senescyt'
+    )
+
+    class Meta:
+        verbose_name = 'título profesional'
+        verbose_name_plural = 'títulos profesionales'
 
     def __str__(self):
         return f'{self.level} - {self.name}'
 
 
 class JobExperience(Timeline):
-    job_position = models.CharField(max_length=64)
-    institution = models.CharField(max_length=128)
-    current_job = models.BooleanField(default=False)
+    institution = models.CharField(max_length=128, verbose_name='institución')
+    job_position = models.CharField(max_length=64, verbose_name='cargo/puesto')
+    current_job = models.BooleanField(
+        default=False,
+        verbose_name='trabajo actual?'
+    )
+
+    class Meta:
+        verbose_name = 'experiencia profesional'
+        verbose_name_plural = 'experiencias profesionales'
 
     def __str__(self):
-        return f'{self.job_position} at {self.institution}'
+        return f'{self.job_position} - {self.institution}'
 
 
 class JobActivity(models.Model):
-    description = models.TextField()
+    description = models.CharField(max_length=64, verbose_name='descripción')
     experience = models.ForeignKey(JobExperience, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'actividad'
+        verbose_name_plural = 'actividades'
