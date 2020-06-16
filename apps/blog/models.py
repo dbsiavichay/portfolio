b6b4258cf2688e64c77c45773eec75f6b5b1d452
Denis Siavichay <dsiavichay@mmorona.gob.ec>
2020-06-15 21:11:30 -0500

    Bio hightlighted at index.html

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,15 @@
+# Django
 from django.db import models
 
-# Create your models here.
+
+class Media(models.Model):
+    file = models.FileField(upload_to='files/%Y/%m/%d/')
+    file_extensionm = models.CharField(max_length=32)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=128)
+    cover = models.ImageField(upload_to='posts')
+    date = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
