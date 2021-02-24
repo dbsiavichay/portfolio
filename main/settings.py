aa3f64af94ad96672461d4bb6a763d05fb68ad37
Denis Siavichay <dbsiavichay@gmail.com>
2021-02-23 20:14:42 -0500

    :wrench: Config for static files

--- main/settings.py
+++ main/settings.py
@@ -139,9 +139,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-)
+]
+
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 
 # Media files
 
